95ad79938c67e32548e740edf99f43657828355a
Khai Do <zaro0508@gmail.com>
2021-07-09 17:21:26 -0700

    Remove region and fix up
    * fix references
    * the region can be set in thee AWS profile therefore it does not
      need to be passed in as a parameter

--- tests/test_buid_expression.py
+++ tests/test_buid_expression.py
@@ -8,35 +8,35 @@ from resolver.rcmd import build_expression
 
 class TestBuildExpression(unittest.TestCase):
 
-    def test_string_input_default_profile_and_region(self):
-        expression = build_expression("my-command", "default-profile", "default-region")
+    def test_string_input_default_profile(self):
+        expression = build_expression("my-command", "default-profile")
         self.assertEqual("my-command", expression)
 
     def test_string_input_no_args(self):
         with pytest.raises(ValueError):
-            build_expression(None, "default-profile", "default-region")
+            build_expression(None, "default-profile")
 
     def test_string_input_empty_args(self):
         with pytest.raises(ValueError):
-            build_expression("", "default-profile", "default-region")
+            build_expression("", "default-profile")
 
     def test_dict_input_no_command(self):
         with pytest.raises(KeyError):
-            build_expression({}, "default-profile", "default-region")
+            build_expression({}, "default-profile")
 
     def test_dict_input_empty_command(self):
         with pytest.raises(ValueError):
             arguments = {
                 "command": ""
             }
-            build_expression(arguments, "default-profile", "default-region")
+            build_expression(arguments, "default-profile")
 
     def test_dict_input_no_overrides(self):
         arguments = {
             "command": "my-command"
         }
-        expression = build_expression(arguments, "default-profile", "default-region")
-        self.assertEqual("AWS_DEFAULT_REGION=default-region AWS_PROFILE=default-profile my-command",
+        expression = build_expression(arguments, "default-profile")
+        self.assertEqual("AWS_PROFILE=default-profile my-command",
                          expression)
 
     def test_dict_input_override_profile(self):
@@ -44,25 +44,6 @@ class TestBuildExpression(unittest.TestCase):
             "command": "my-command",
             "profile": "my-profile"
         }
-        expression = build_expression(arguments, "default-profile", "default-region")
-        self.assertEqual("AWS_DEFAULT_REGION=default-region AWS_PROFILE=my-profile my-command",
-                         expression)
-
-    def test_dict_input_override_region(self):
-        arguments = {
-            "command": "my-command",
-            "region": "my-region"
-        }
-        expression = build_expression(arguments, "default-profile", "default-region")
-        self.assertEqual("AWS_DEFAULT_REGION=my-region AWS_PROFILE=default-profile my-command",
-                         expression)
-
-    def test_dict_input_override_profile_and_region(self):
-        arguments = {
-            "command": "my-command",
-            "profile": "my-profile",
-            "region": "my-region"
-        }
-        expression = build_expression(arguments, "default-profile", "default-region")
-        self.assertEqual("AWS_DEFAULT_REGION=my-region AWS_PROFILE=my-profile my-command",
+        expression = build_expression(arguments, "default-profile")
+        self.assertEqual("AWS_PROFILE=my-profile my-command",
                          expression)
