6b4a3b71bdbd9a9150b8d039fe359dedf44c1823
Khai Do <zaro0508@gmail.com>
2024-02-15 10:58:32 -0800

    move to github actions
    Update with files from the  sceptre-resolver-template[1]
    repo to update this project to use github actions CI
    
    [1] https://github.com/Sceptre/sceptre-resolver-template

--- tests/test_rcmd.py
+++ tests/test_rcmd.py
@@ -13,18 +13,16 @@ from resolver.rcmd import SceptreResolverCmd
 
 
 class TestSceptreResolverCmd(TestCase):
-
     def setUp(self):
         self.argument = 'echo "hello!"'
-        self.envs = {
-            'SHELL': '/my/special/shell'
-        }
+        self.envs = {"SHELL": "/my/special/shell"}
 
         self.stack = Mock(
             Stack,
-            connection_manager=Mock(ConnectionManager, **{
-                'create_session_environment_variables.return_value': self.envs
-            })
+            connection_manager=Mock(
+                ConnectionManager,
+                **{"create_session_environment_variables.return_value": self.envs}
+            ),
         )
         self.stack.name = "mock-name"
         self.subprocess_run = create_autospec(subprocess.run)
@@ -32,18 +30,16 @@ class TestSceptreResolverCmd(TestCase):
     @property
     def resolver(self):
         return SceptreResolverCmd(
-            argument=self.argument,
-            stack=self.stack,
-            subprocess_run=self.subprocess_run
+            argument=self.argument, stack=self.stack, subprocess_run=self.subprocess_run
         )
 
     def test_resolve__dict_arg__runs_command_from_dict_arg(self):
-        self.argument = {'command': self.argument}
+        self.argument = {"command": self.argument}
 
         self.resolver.resolve()
 
         self.subprocess_run.assert_any_call(
-            [self.envs['SHELL'], '-c', self.argument['command']],
+            [self.envs["SHELL"], "-c", self.argument["command"]],
             env=self.envs,
             check=True,
             capture_output=True,
@@ -53,32 +49,36 @@ class TestSceptreResolverCmd(TestCase):
         self.resolver.resolve()
 
         self.subprocess_run.assert_any_call(
-            [self.envs['SHELL'], '-c', self.argument],
+            [self.envs["SHELL"], "-c", self.argument],
             env=self.envs,
             check=True,
             capture_output=True,
         )
 
     def test_resolve__dict_arg__no_connection_params__uses_stack_default(self):
-        self.argument = {'command': self.argument}
+        self.argument = {"command": self.argument}
 
         self.resolver.resolve()
-        expected_profile = expected_region = expected_role = self.stack.connection_manager.STACK_DEFAULT
+        expected_profile = (
+            expected_region
+        ) = expected_role = self.stack.connection_manager.STACK_DEFAULT
         self.stack.connection_manager.create_session_environment_variables.assert_any_call(
             expected_profile, expected_region, expected_role
         )
 
     def test_resolve__dict_arg__takes_connection_params_from_dict(self):
         self.argument = {
-            'command': self.argument,
-            'profile': 'my_profile',
-            'region': 'my_region',
-            'sceptre_role': 'my_role'
+            "command": self.argument,
+            "profile": "my_profile",
+            "region": "my_region",
+            "sceptre_role": "my_role",
         }
 
         self.resolver.resolve()
         self.stack.connection_manager.create_session_environment_variables.assert_any_call(
-            self.argument['profile'], self.argument['region'], self.argument['sceptre_role']
+            self.argument["profile"],
+            self.argument["region"],
+            self.argument["sceptre_role"],
         )
 
     def test_resolve__returns_output(self):
@@ -87,27 +87,33 @@ class TestSceptreResolverCmd(TestCase):
         self.assertEqual(self.subprocess_run.return_value.stdout, result)
 
     def test_resolve__output_is_bytes__decodes_using_shell_encoding(self):
-        expected = 'hello!'
-        self.envs['SHELL_ENCODING'] = encoding = 'utf-32'
+        expected = "hello!"
+        self.envs["SHELL_ENCODING"] = encoding = "utf-32"
         self.subprocess_run.return_value.stdout = expected.encode(encoding)
         result = self.resolver.resolve()
 
         self.assertEqual(expected, result)
 
-    def test_resolve__output_is_bytes_but_shell_encoding_not_set__deocodes_via_utf8(self):
-        expected = 'hello!'
-        self.subprocess_run.return_value.stdout = expected.encode('utf-8')
+    def test_resolve__output_is_bytes_but_shell_encoding_not_set__deocodes_via_utf8(
+        self,
+    ):
+        expected = "hello!"
+        self.subprocess_run.return_value.stdout = expected.encode("utf-8")
         result = self.resolver.resolve()
 
         self.assertEqual(expected, result)
 
-    @parameterized.expand([
-        ('no arg', None),
-        ('dict arg with no command', {}),
-        ('dict arg with empty command', {'command': ''}),
-        ('string arg with empty command', '')
-    ])
-    def test_resolve__invalid_command_argument__raises_sceptre_exception(self, name, arg):
+    @parameterized.expand(
+        [
+            ("no arg", None),
+            ("dict arg with no command", {}),
+            ("dict arg with empty command", {"command": ""}),
+            ("string arg with empty command", ""),
+        ]
+    )
+    def test_resolve__invalid_command_argument__raises_sceptre_exception(
+        self, name, arg
+    ):
         self.argument = arg
 
         with self.assertRaises(SceptreException):
