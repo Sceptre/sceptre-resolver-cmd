f89974863e2285ebf35ab851a734bf0642d401f2
Khai Do <3697686+zaro0508@users.noreply.github.com>
2024-02-15 16:13:38 -0800

    move to github actions (#15)
    Update with files from the  sceptre-resolver-template[1]
    repo to update this project to use github actions CI
    
    [1] https://github.com/Sceptre/sceptre-resolver-template

--- resolver/rcmd.py
+++ resolver/rcmd.py
@@ -24,34 +24,36 @@ class SceptreResolverCmd(Resolver):
         :return: the resulting output from the executed command
         """
         command, environment_variables = self._get_subprocess_envs()
-        shell = environment_variables.get('SHELL', '/bin/bash')
+        shell = environment_variables.get("SHELL", "/bin/bash")
         result = self._subprocess_run(
-            [shell, '-c', command],
+            [shell, "-c", command],
             env=environment_variables,
             check=True,
-            capture_output=True
+            capture_output=True,
         )
         output = result.stdout
 
         if isinstance(output, bytes):
-            output = output.decode(environment_variables.get('SHELL_ENCODING', 'utf-8'))
+            output = output.decode(environment_variables.get("SHELL_ENCODING", "utf-8"))
 
         return output
 
     def _get_subprocess_envs(self):
         profile = region = role = self.stack.connection_manager.STACK_DEFAULT
         if isinstance(self.argument, dict):
-            profile = self.argument.get('profile', profile)
-            region = self.argument.get('region', region)
-            role = self.argument.get('sceptre_role', role)
-            command: Optional[str] = self.argument.get('command', '')
+            profile = self.argument.get("profile", profile)
+            region = self.argument.get("region", region)
+            role = self.argument.get("sceptre_role", role)
+            command: Optional[str] = self.argument.get("command", "")
         else:
             command: str = self.argument
 
-        if command in ('', None):
+        if command in ("", None):
             raise SceptreException("A command is required for the !rcmd resolver")
 
-        environment_variables = self.stack.connection_manager.create_session_environment_variables(
-            profile, region, role
+        environment_variables = (
+            self.stack.connection_manager.create_session_environment_variables(
+                profile, region, role
+            )
         )
         return command, environment_variables
